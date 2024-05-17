a1ac5400023a49168e947fa608cbabc4cd9671fd
ayewhosthat <alan.notiverson@gmail.com>
2024-05-17 15:05:16 -0400

    Add playGame function, as well as configure the script to play 5 rounds of rock paper scissors, outputting the final scores at the end

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
     let randomNumber = Math.random();
     let computerChoice;
@@ -21,7 +18,38 @@ function getHumanChoice() {
     return playerChoice;
 }
 
-function playRound(humanChoice, computerChoice) {
-    let humanChoiceLower = humanChoice.toLowerCase();
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
     
+    for (let i = 1; i <= 5; i++) {
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+        
+        function playRound(humanChoice, computerChoice) {
+        let humanChoiceLower = humanChoice.toLowerCase();
+
+        let winCondition1 = (humanChoiceLower == "paper") && (computerChoice == "rock");
+        let winCondition2 = (humanChoiceLower == "rock") && (computerChoice == "scissors");
+        let winCondition3 = (humanChoiceLower == "scissors") && (computerChoice = "paper");
+
+        let loseCondition1 = (humanChoiceLower == "paper") && (computerChoice == "scissors");
+        let loseCondition2 = (humanChoiceLower == "rock") && (computerChoice == "paper");
+        let loseCondition3 = (humanChoiceLower == "scissors") && (computerChoice == "rock");
+
+        if (winCondition1 || winCondition2 || winCondition3) {
+            humanScore += 1;
+            console.log(`You won! ${humanChoiceLower} beats ${computerChoice}`);
+        } else if (loseCondition1 || loseCondition2 || loseCondition3) {
+            computerScore += 1;
+            console.log(`You lost! ${computerChoice} beats ${humanChoiceLower}`);
+        } else {
+            console.log(`It's a draw!`);
+        }
+        }
+        
+        playRound(humanChoice, computerChoice);
+    }
+
+    console.log(`Final score: You: ${humanScore} vs Computer: ${computerScore}`);
 }
